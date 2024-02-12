11ce79fd89c8c8e7470dd43596a986cda11707d2
dixiNourmus <joshua.berger@talawanda.org>
2024-02-12 14:04:58 -0500

    fdg

---
+++ src/you_think_you_the_shit.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class you_think_you_the_shit {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String fName = "";
+        String lName = "";
+        System.out.println("whats yo name?");
+        fName = scanner.nextLine();
+        System.out.println("whats yo last name foo?");
+        lName = scanner.nextLine();
+        System.out.println("yo full name be " + fName + " " + lName);
+
+
+    }
+}
