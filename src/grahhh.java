25b1b74d104104f52bf4f71ade8b5fa88bb5de8b
dixiNourmus <joshua.berger@talawanda.org>
2024-02-13 14:06:11 -0500

    fdg

---
+++ src/grahhh.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class grahhh {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num1 = 0;
+        String trash = "";
+        System.out.println("give me an int");
+        if (scanner.hasNextInt()){
+            if (num1 > 0) {
+
+            }
+
+
+        }
+
+
+
+    }
+
+}
